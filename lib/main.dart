e45de3ed8bedd07774d1479f3d742a17422cd625
hussamburqan <116378522+hussamburqan@users.noreply.github.com>
2023-11-20 00:16:51 +0200

    commit 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'Data/Weather Data.dart';
 import 'Screens/HomeScreen.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MyApp());
@@ -13,18 +16,58 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-
-          title: Text('Fuck Hussam'),
+          centerTitle: true,
+          title: const Text('Weather App'),
         ),
-        body: const HomeScreen(),
+
+
+        body: HomeScreen(),
       ),
     );
+  }
+
+ //  FutureBuilder(future: FutureWeatherData,
+ //  builder: (context,snapshot) {
+ //  if(snapshot.hasData){
+ //  return const Text('Data is ready');
+ //  }
+ // else if(snapshot.hasError){
+ //  return const Text('Data not ready');}
+ // else {
+ // return const Center(
+ // child: CircularProgressIndicator(),
+ // );}
+ // }
+ // ),
+
+late Future<WeatherData> FutureWeatherData;
+
+  Future<WeatherData> getWeatherData() async {
+
+    http.Response response = await http.get(Uri.parse('http://api.weatherapi.com/v1/current.json?key=79fb3945227647ad94595829231911&q=London&aqi=no'));
+
+    if(response.statusCode== 200){
+
+    var jsonObject = jsonDecode(response.body);
+
+    WeatherData data = WeatherData.fromJson(jsonObject);
+
+    return data;
+    }
+    else {
+      throw Exception('cant get Data ');
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    FutureWeatherData = getWeatherData();
   }
 }
